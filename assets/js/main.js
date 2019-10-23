cd31eb0c368bec16d71d1eb294c5828330ecbc55
EmilieLamps <emilie.lamps@gmail.com>
2019-10-23 16:20:46 +0200

    Premier envoi

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function formulaire (){
+let lastName = document.getElementById("lastName").value;
+let firstName = document.getElementById("firstName").value;
+let city = document.getElementById("city").value;
+alert("Nom : " + lastName + "\nPrénom : " + firstName + "\nVille : " + city);
+// alert("Votre nom : "+ nom +"\nVotre prénom : "+ prenom +"\nVotre ville : "+ ville);
+}
